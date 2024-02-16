b105eae4a60b8b65adbcb3c8c338670f3378d22b
Abdulkadir Karatas <abdulkadirkaratas55@gmail.com>
2024-02-16 17:47:37 +0300

    1.14

--- reverse_bits/reverse_bits.c
+++ reverse_bits/reverse_bits.c
@@ -1,14 +1,13 @@
 
 unsigned char reverse_bits(unsigned char octet)
 {
-	int shiftnum = 7;
+	int shiftnum = 0;
 	unsigned char res = 0;
-	while (shiftnum >= 0)
+	while (shiftnum <= 7)
 	{
 		unsigned char bit = (octet >> shiftnum) & 1;
 		res = (res << 1) + bit;
-		shiftnum--;
+		shiftnum++;
 	}
-
 	return (res);
 }
