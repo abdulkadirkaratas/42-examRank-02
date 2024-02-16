2723d59abcfea3302616a164bc1f22186ce306b1
Abdulkadir Karatas <abdulkadirkaratas55@gmail.com>
2024-02-16 23:07:55 +0300

    1.9

---
+++ inter/inter.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+
+int str1control(char *str1, int index)
+{
+	int i = 0;
+	while (i < index)
+	{
+		if (str1[i] == str1[index])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int str2control(char c, char *str2)
+{
+	int i = 0;
+	while (str2[i] != 0)
+	{
+		if (c == str2[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 3)
+	{
+		char *str1 = argv[1];
+		char *str2 = argv[2];
+
+		int i = 0;
+		while (str1[i] != 0)
+		{
+			if (str1control(str1, i) == 1 && str2control(str1[i], str2) == 1)
+				write(1, &str1[i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+}
